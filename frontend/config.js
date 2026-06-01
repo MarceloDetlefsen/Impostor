a70b4065d95a136bbf91c581ac08f58441c08362
Marcelo Detlefsen <marcelodetlefsen1a@gmail.com>
2026-06-01 00:35:21 -0600

    Se actualiza el deploy del backend a Render.

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,8 @@
 // Local
 // window.IMPOSTOR_API_URL = "http://localhost:3000";
 
-// Production
-window.IMPOSTOR_API_URL = "https://impostor-production-e89b.up.railway.app";
+// Production - Railway
+// window.IMPOSTOR_API_URL = "https://impostor-production-e89b.up.railway.app";
+
+// Production - Render
+window.IMPOSTOR_API_URL = "https://impostor-yhkz.onrender.com";
